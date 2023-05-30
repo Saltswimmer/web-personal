70b3afcb10fc59cf338ee4157c23e6a7697bc66b
Saltswimmer <ethanciavo@gmail.com>
2023-05-30 15:03:59 -0400

    Added video and image to modals

--- public/projects/project-2.md
+++ public/projects/project-2.md
@@ -1,16 +1,18 @@
 ## Ochre
 
-Ochre is a 2D platformer I created for a game design elective I took in my senior year.
+![Image](ochre_0.png "Ochre screenshot")
+
+Ochre is a 2D platformer I created in Unreal Engine for a game design elective I took in my senior year.
 
 ### How it was built
 
-There are essentially two ways to make a game in Unreal Engine: writing native code using C++, and using visual scripting (called 'blueprints'). For this class, we used blueprints.
+There are essentially two ways to make a game in Unreal Engine 4: writing native code using C++, and using visual scripting (called 'blueprints'). For this class, we used blueprints.
 
-(image)
+![Image](ochre_1.png "Blueprint editor")
 
-The pixel art graphics were drawn using a program called Aseprite.
+I drew the pixel art graphics using a program called Aseprite.
 
-(Video demo?)
+![Image](ochre_2.png "Aseprite screenshot")
 
 ### Project status
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/prop-types */
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { generate } from "shortid";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import { IoLogoReact } from "react-icons/io5";
 import { BsGithub } from "react-icons/bs";
+import { MdArrowBack, MdClose, MdArrowForward } from "react-icons/md";
 
 function Navlink({title, section, disable=false}) {
 
@@ -28,8 +29,8 @@ function Sidebar() {
             <a 
                 style={{paddingRight: '16px'}}
                 href='https://www.linkedin.com/in/ethan-ciavolella-962432193/'
-            ><img width='32px' src='li.svg'></img></a>
-            <a href='https://github.com/Saltswimmer/'><img width='32px' src='github-mark-white.svg'></img></a>
+            ><img width='32px' src='li.svg' alt='My LinkedIn'></img></a>
+            <a href='https://github.com/Saltswimmer/'><img width='32px' src='github-mark-white.svg' alt='My GitHub page'></img></a>
         </div>
     </div>
 }
@@ -61,10 +62,12 @@ function Navbar() {
     
 }
 
-function ProjectModal({texturl}) {
+function ProjectModal({texturl, videoEmbed, onClose}) {
     const [text, setText] = useState('');
+    const dialog = useRef(null);
 
     useEffect(() => {
+
         let fetchText = async function () {
             try {
                 const response = await fetch(texturl);
@@ -79,21 +82,48 @@ function ProjectModal({texturl}) {
 
     return <div className="Isolate"><dialog
         id={texturl}
-        onClose={() => document.body.classList.remove('NoScroll')}
+        onClose={onClose}
+        ref={dialog}
     >
+        <div tabIndex={0} />
+        <div style={{display: 'flex', justifyContent: 'space-around', position: "sticky", background: "white"}}>
+            <MdArrowBack tabIndex={1}/>
+            <MdClose tabIndex={2} onClick={()=>dialog.current.close()}/>
+            <MdArrowForward tabIndex={3} onClick={()=>{}}/>
+        
+        </div>
         <ReactMarkdown>{text}</ReactMarkdown>
+        {videoEmbed}
     </dialog></div>
 }
 
-function Project({src, caption, texturl}) {
+function Project({src, caption, texturl, children}) {
+    const [open, setOpen] = useState(false);
+    const modal = useRef(null);
+
+    let handleClose = () => {
+        setOpen(false);
+        document.body.classList.remove('NoScroll');
+    }
+
+    useEffect(() => {
+        if (open) {
+            document.addEventListener('click', (e) => {
+                if (modal.current && !modal.current.contains(e.target)) {
+                    document.getElementById(texturl).close();
+                }}
+            )
+        }
+    }, [open, texturl])
 
     return <span className='Project' onClick={() => {
             document.getElementById(texturl).showModal();
             document.body.classList.add('NoScroll');
+            setOpen(true);
         }}>
         <img width={625} src={src} alt={caption}/>
         <h2>{caption}</h2>
-        <ProjectModal texturl={texturl}/>
+        <ProjectModal ref={modal} texturl={texturl} videoEmbed={children} onClose={handleClose}/>
     </span>;
 }
 
@@ -111,8 +141,52 @@ export default function Index() {
             <h1>About me</h1>
             <p>
                 I earned my bachelor&apos;s degree in computer science
-                at Rowan University in May 2023.
-                <br></br>
+                at Rowan University in May 2023. The languages I am most proficient at
+                include:
+                <ul>
+                    <li>Java</li>
+                    <li>Javascript</li>
+                    <li>C++</li>
+                    <li>Python</li>
+                </ul><br></br>
+                Below are some libraries and platforms I&apos;ve used over the years:
+                <ul>
+                    <li>Web and UI design
+                        <ul>
+                            <li>React</li>
+                            <li>CSS</li>
+                            <li>JavaFX</li>
+                        </ul>
+                    </li>
+                    <li>Databases
+                        <ul>
+                            <li>MySQL</li>
+                            <li>PostgreSQL</li>
+                        </ul>
+                    </li>
+                    <li>Scientific
+                        <ul>
+                            <li>Matplotlib</li>
+                            <li>MATLAB</li>
+                        </ul>
+                    </li>
+                    <li>Game development
+                        <ul>
+                            <li>Unity</li>
+                            <li>Unreal Engine 4</li>
+                            <li>Godot</li>
+                        </ul>
+                    </li>
+                </ul> 
+            </p>
+            <h2>Awards and Accomplishments</h2>
+            <p>
+                <ul>
+                    <li>B.S. Computer Science, 3.9/4.0 GPA</li>
+                    <li>John H. Martinson Honors College graduate</li>
+                    <li>2x Rowan University President&apos;s Scholars of Excellence List</li>
+                    <li>7x Rowan University Dean&apos;s List</li>
+                </ul>
             </p>
             <br></br>
             <h2>About this site</h2>
@@ -124,7 +198,12 @@ export default function Index() {
         <div id='projects' className="Section">
             <h1>Projects</h1>
             <Project id={generate()} src='harbour.png' caption='Harbour' texturl='/projects/project-1.md'/><br></br>
-            <Project id={generate()} src='harbour.png' caption='Ochre' texturl='/projects/project-2.md'/><br></br>
+            <Project id={generate()} src='ochre_0.png' caption='Ochre' texturl='/projects/project-2.md'>
+                <video controls loop width='500px'>
+                    <source src='ochre_footage.mp4' type='video/mp4'/>
+                </video>
+            </Project>
+            <br></br>
             <Project id={generate()} src='harbour.png' caption='Project Cranefly' texturl='/projects/project-3.md'/><br></br>
         </div>
     </>
